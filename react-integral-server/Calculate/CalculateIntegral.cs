b1c1afad6c77525f0957cb77e768faef8f01924f
EgasVegas <egor1109_45@mail.ru>
2022-11-01 18:40:46 +0300

    Initial commit

---
+++ react-integral-server/Calculate/CalculateIntegral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace react_integral_server.Calculate
+{
+    public class CalculateIntegral
+    {
+        public double f(double x)
+        {
+            double res;
+            res = Math.Pow(2, 3 * x);
+            return res;
+        }
+
+        public double Calculate(double upperInt, double lowerInt, double stepInt)
+        {
+            double h = (upperInt-lowerInt)/stepInt;
+            double s = 0;
+            int i = 0;
+                for (i = 0; i < stepInt; i++)
+                {
+                    s += h * f(lowerInt + h * i);
+                }
+            return s;
+        }
+    }
+}
+
---
+++ react-integral-server/Controllers/IntegralSolution.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using react_integral_server.Calculate;
+
+namespace react_integral_server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IntegralSolution : ControllerBase
+{
+    [HttpPost("calculate")]
+    public ActionResult<double> Calculate(double upperInt, double lowerInt, double stepInt)
+    {
+        double result;
+        CalculateIntegral calculate = new CalculateIntegral();
+        result = calculate.Calculate(upperInt,lowerInt,stepInt);
+        return Ok("Результат " + result);
+    }
+}
+
